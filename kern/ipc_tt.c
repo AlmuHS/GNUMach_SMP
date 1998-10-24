abdaaef19b3d876f2f77b1be71325d0469d578f1
Roland McGrath <roland@gnu.org>
1998-10-24 18:21:14 +0000

    1998-10-24  Roland McGrath  <roland@baalperazim.frob.com>
    	* kern/ipc_tt.c (mach_ports_lookup): Remove unnecessary cast.

--- kern/ipc_tt.c
+++ kern/ipc_tt.c
@@ -1,25 +1,25 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990,1989,1988,1987 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
@@ -1206,7 +1206,7 @@ mach_ports_lookup(task, portsp, portsCnt)
 
 	itk_unlock(task);
 
-	*portsp = (mach_port_array_t) ports;
+	*portsp = ports;
 	*portsCnt = TASK_PORT_REGISTER_MAX;
 	return KERN_SUCCESS;
 }
@@ -1332,7 +1332,7 @@ convert_port_to_thread(port)
  *	Purpose:
  *		Convert from a task to a port.
  *		Consumes a task ref; produces a naked send right
- *		which may be invalid.  
+ *		which may be invalid.
  *	Conditions:
  *		Nothing locked.
  */
