dd267ba547c2a922656528e11ed83b605cee0052
Roland McGrath <roland@gnu.org>
2002-04-23 21:31:06 +0000

    2002-04-23  Roland McGrath  <roland@frob.com>
    	* i386/i386/ldt.c (ldt_init): Fix fencepost error in segment limits.

--- i386/i386/ldt.c
+++ i386/i386/ldt.c
@@ -1,25 +1,25 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
@@ -51,14 +51,15 @@ ldt_init()
 		      (vm_offset_t)&syscall, KERNEL_CS,
 		      ACC_PL_U|ACC_CALL_GATE, 0);
 	fill_ldt_descriptor(USER_CS,
-			    VM_MIN_ADDRESS, VM_MAX_ADDRESS-VM_MIN_ADDRESS,
+			    VM_MIN_ADDRESS,
+			    VM_MAX_ADDRESS-VM_MIN_ADDRESS-4096,
 			    /* XXX LINEAR_... */
 			    ACC_PL_U|ACC_CODE_R, SZ_32);
 	fill_ldt_descriptor(USER_DS,
-			    VM_MIN_ADDRESS, VM_MAX_ADDRESS-VM_MIN_ADDRESS,
+			    VM_MIN_ADDRESS,
+			    VM_MAX_ADDRESS-VM_MIN_ADDRESS-4096,
 			    ACC_PL_U|ACC_DATA_W, SZ_32);
 
 	/* Activate the LDT.  */
 	lldt(KERNEL_LDT);
 }
-
