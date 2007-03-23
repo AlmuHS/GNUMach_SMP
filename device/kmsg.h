0bcc497664baaecb2e8a9873d61201ddbfddf3cc
Thomas Schwinge <tschwinge@gnu.org>
2007-03-23 23:57:22 +0000

    2007-03-24  Thomas Schwinge  <tschwinge@gnu.org>
    * device/kmsg.h: Include <sys/types.h>, <device/device_types.h> and
    	<device/io_req.h>.

--- device/kmsg.h
+++ device/kmsg.h
@@ -3,6 +3,11 @@
 
 #ifdef MACH_KERNEL
 
+#include <sys/types.h>
+
+#include <device/device_types.h>
+#include <device/io_req.h>
+
 io_return_t kmsgopen (dev_t dev, int flag, io_req_t ior);
 io_return_t kmsgclose (dev_t dev, int flag);
 io_return_t kmsgread (dev_t dev, io_req_t ior);
