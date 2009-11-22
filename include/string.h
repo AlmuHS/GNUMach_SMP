8d4fdfbe43628119fc87920239704fe3d240d8a2
Samuel Thibault <samuel.thibault@ens-lyon.org>
2009-11-22 22:03:20 +0100

    Declare memcmp function
    	* include/string.h (memcmp): New prototype.

--- include/string.h
+++ include/string.h
@@ -30,6 +30,8 @@
 
 extern void *memcpy (void *dest, const void *src, size_t n);
 
+extern int *memcmp (const void *s1, const void *s2, size_t n);
+
 extern void *memset (void *s, int c, size_t n);
 
 extern char *strchr (const char *s, int c);
