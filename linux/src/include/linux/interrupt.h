AlmuHS/GNUMach_SMP

commit 5f701793f63f16d111db3e3d2e91134fcc179c5b

Fix variable types according to assembly use
* linux/dev/arch/i386/kernel/irq.c (intr_count): Set to int type.
* linux/dev/kernel/softirq.c (bh_active, bh_mask): Likewise.
(linux_soft_intr: active, mask, left): Likewise
* linux/src/include/linux/interrupt.h (bh_active, bh_mask): Likewise.
* linux/src/kernel/softirq.c (intr_count, bh_active, bh_mask): Likewise.
(do_bottom_half: active, mask, left): Likewise

linux/src/include/linux/interrupt.h
@@ -17,8 +17,8 @@ struct irqaction {
 extern unsigned long intr_count;
 
 extern int bh_mask_count[32];
-extern unsigned long bh_active;
-extern unsigned long bh_mask;
+extern unsigned int bh_active;
+extern unsigned int bh_mask;
 extern void (*bh_base[32])(void);
 
 asmlinkage void do_bottom_half(void);

linux/dev/arch/i386/kernel/irq.c
@@ -72,7 +72,7 @@ spl_t linux_intr_pri;
 /*
  * Flag indicating an interrupt is being handled.
  */
-unsigned long intr_count = 0;
+unsigned int intr_count = 0;
 
 /*
  * List of Linux interrupt handlers.

linux/dev/kernel/softirq.c
@@ -16,15 +16,15 @@
 #include <linux/dev/glue/glue.h>
 
 int bh_mask_count[32];
-unsigned long bh_active = 0;
-unsigned long bh_mask = 0;
+unsigned int bh_active = 0;
+unsigned int bh_mask = 0;
 void (*bh_base[32]) (void);
 
 void
 linux_soft_intr (void)
 {
-  unsigned long active;
-  unsigned long mask, left;
+  unsigned int active;
+  unsigned int mask, left;
   void (**bh) (void);
 
   sti ();

linux/src/kernel/softirq.c
@@ -21,18 +21,18 @@
 #include <asm/irq.h>
 #include <asm/bitops.h>
 
-unsigned long intr_count = 0;
+unsigned int intr_count = 0;
 
 int bh_mask_count[32];
-unsigned long bh_active = 0;
-unsigned long bh_mask = 0;
+unsigned int bh_active = 0;
+unsigned int bh_mask = 0;
 void (*bh_base[32])(void);
 
 
 asmlinkage void do_bottom_half(void)
 {
-	unsigned long active;
-	unsigned long mask, left;
+	unsigned int active;
+	unsigned int mask, left;
 	void (**bh)(void);
 
 	sti();

commit caaf3ece6d895b326f9b52df207b26d5dc0566f7

Fix build error introduced in 5f701793f63f16d111db3e3d2e91134fcc179c5b.
* linux/src/include/linux/interrupt.h (intr_count): Declare as unsigned int.

linux/src/include/linux/interrupt.h
@@ -14,7 +14,7 @@ struct irqaction {
 	struct irqaction *next;
 };
 
-extern unsigned long intr_count;
+extern unsigned int intr_count;
 
 extern int bh_mask_count[32];
 extern unsigned int bh_active;