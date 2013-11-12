e60639327a9233b8e2ec5c6303a279145dae9f99
Marin Ramesa <mpr@hi.t-com.hr>
2013-11-12 11:25:28 +0100

    kern: remove register qualifiers
    * kern/priority.c: Remove register qualifiers.

--- kern/priority.c
+++ kern/priority.c
@@ -75,15 +75,15 @@
  */
 
 void thread_quantum_update(mycpu, thread, nticks, state)
-	register int		mycpu;
-	register thread_t	thread;
+	int			mycpu;
+	thread_t		thread;
 	int			nticks;
 	int			state;
 {
-	register int			quantum;
-	register processor_t		myprocessor;
+	int				quantum;
+	processor_t			myprocessor;
 #if	NCPUS > 1
-	register processor_set_t	pset;
+	processor_set_t			pset;
 #endif
 	spl_t				s;
 
