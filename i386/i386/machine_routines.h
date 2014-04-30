4fda696b13d2d9fdb9496b2aeee1b05a6f852836
Justus Winter <4winter@informatik.uni-hamburg.de>
2014-04-30 17:31:49 +0200

    i386: fix MACHINE_SERVER_HEADER
    Commit b6dab094 introduced a way to include the MIG-generated server
    files for the machine specific interface in ipc_kobject.c.
    
    This broke out-of-tree builds.  Here, 'machine' is a symlink to
    '../i386/i386', it points into the source tree.  The MIG-generated
    files however are put in the build tree in i386/i386.
    
    * i386/i386/machine_routines.h (MACHINE_SERVER_HEADER): Fix path.

--- i386/i386/machine_routines.h
+++ i386/i386/machine_routines.h
@@ -31,7 +31,7 @@
  * The i386 has a set of machine-dependent interfaces.
  */
 #define	MACHINE_SERVER		mach_i386_server
-#define	MACHINE_SERVER_HEADER	<machine/mach_i386.server.h>
+#define	MACHINE_SERVER_HEADER	"i386/i386/mach_i386.server.h"
 #define	MACHINE_SERVER_ROUTINE	mach_i386_server_routine
 
 #endif /* _I386_MACHINE_ROUTINES_H_ */
