23bda7b9566982d304ff7d51fa1c1ab8da41c99b
Justus Winter <justus@gnupg.org>
2016-05-20 00:31:44 +0200

    xen: fix prototype
    Amends a7f248bf.
    
    * xen/console.h (hypcnclose): Fix type of parameter.

--- xen/console.h
+++ xen/console.h
@@ -42,7 +42,7 @@ extern int hypcninit(struct consdev *cp);
 extern int hypcnopen(dev_t dev, int flag, io_req_t ior);
 extern int hypcnread(dev_t dev, io_req_t ior);
 extern int hypcnwrite(dev_t dev, io_req_t ior);
-extern void hypcnclose(int dev, int flag);
+extern void hypcnclose(dev_t dev, int flag);
 extern io_return_t hypcngetstat(dev_t dev, int flavor, int *data, unsigned int *count);
 extern io_return_t hypcnsetstat(dev_t dev, int flavor, int *data, unsigned int count);
 extern int hypcnportdeath(dev_t dev, mach_port_t port);
