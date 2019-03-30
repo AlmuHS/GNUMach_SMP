4e3f897bab32037e65cd6a247de54388cb123ca4
AlmuHS <liberamenso10000@gmail.com>
2019-03-30 13:14:18 +0100

    added ncpu > 1 control before extra_setup()

--- kern/startup.c
+++ kern/startup.c
@@ -130,7 +130,7 @@ void setup_main(void)
          * After virtual memory is up, do extra initializations:
          * currently it maps LAPIC (in acpi_rsdp.c)
          */
-        extra_setup();
+        if(ncpu > 1) extra_setup();
 
 	init_timers();
 	init_timeout();
