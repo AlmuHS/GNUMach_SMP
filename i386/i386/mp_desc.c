49e9feec3a0dc099a058bbe7376818e68e5ff6d3
AlmuHS <liberamenso10000@gmail.com>
2019-04-03 00:19:58 +0200

    try to fix stack error

--- i386/i386/mp_desc.c
+++ i386/i386/mp_desc.c
@@ -499,8 +499,8 @@ start_other_cpus(void)
 
 	//Initialize cpu stack
 	#define STACK_SIZE (4096 * 2)
-	stack_ptr = (void*) kalloc(STACK_SIZE);
-  printf("stack: %x\n", *stack_ptr);
+	*stack_ptr = (void*) kalloc(STACK_SIZE);
+  printf("stack: %x\n", stack_ptr);
 
 	for (cpu = 0; cpu < ncpu; cpu++){
 		if (cpu != cpu_number()){
