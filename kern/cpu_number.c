93a73ecd80568d8d139d9d8783305105c2e66357
AlmuHS <almuhs@github.com>
2019-04-17 21:14:40 +0200

    recovered lapic check in cpu_number()

--- kern/cpu_number.c
+++ kern/cpu_number.c
@@ -8,7 +8,7 @@ unsigned int master_cpu = 0;	/* 'master' processor - keeps time */
 int
 cpu_number()
 {
-	if(ncpu == 1) return 0;
+	if(lapic == 0 | ncpu == 1) return 0;
 	else{ 
 		unsigned apic_id = lapic->apic_id.r >>24;
 
