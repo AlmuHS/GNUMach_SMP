e4ad97606736ae581acf7b1c61af7f1ad47b6bf5
AlmuHS <almuhs@github.com>
2019-04-17 21:08:42 +0200

    recover *lapic in cpu_number()

--- kern/cpu_number.c
+++ kern/cpu_number.c
@@ -2,19 +2,15 @@
 #include <i386at/acpi_rsdp.h>
 #include <imps/apic.h>
 #include <mach/machine.h>
-#include <i386/vm_param.h> //phystokv
 
 unsigned int master_cpu = 0;	/* 'master' processor - keeps time */
-
-extern int lapic_addr;
 
 int
 cpu_number()
 {
 	if(ncpu == 1) return 0;
 	else{ 
-		//unsigned apic_id = lapic->apic_id.r >>24;
-		unsigned apic_id = (((ApicLocalUnit*)phystokv(lapic_addr))->apic_id.r >> 24) & 0xff;
+		unsigned apic_id = lapic->apic_id.r >>24;
 
 		return apic2kernel[apic_id];
 
