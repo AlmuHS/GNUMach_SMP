860c89424044bc1059984fa8089774f78a242e3b
AlmuHS <almuhs@github.com>
2019-06-26 17:07:21 +0200

    debug fix: replace true to 1

--- kern/startup.c
+++ kern/startup.c
@@ -183,7 +183,7 @@ void setup_main(void)
 	(void) thread_create(kernel_task, &startup_thread);
 	thread_start(startup_thread, start_kernel_threads);
 
-	while(true);
+	while(1);
 
 	/*
 	 * Give it a kernel stack.
