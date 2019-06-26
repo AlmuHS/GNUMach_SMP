017ddee48d613d6710b43ce7e7a281188c7f68d3
AlmuHS <almuhs@github.com>
2019-06-26 23:54:33 +0200

    debug: hardcoded apic_id = 1 in AP processor

--- i386/i386/mp_desc.c
+++ i386/i386/mp_desc.c
@@ -303,8 +303,8 @@ cpu_setup()
     /* assume Pentium 4, Xeon, or later processors */
     //unsigned apic_id = (((ApicLocalUnit*)phystokv(lapic_addr))->apic_id.r >> 24) & 0xff;
     //unsigned apic_id = lapic->apic_id.r >> 24;
-    unsigned apic_id = 1;
-    printf("apic id: %d", apic_id);
+    unsigned apic_id = 1 >> 24;
+    printf("apic id: %d\n", apic_id);
 
     /* panic? */
     if(i >= ncpu)
