a20a8290ea8198c9e13eced2cffb79e6b89f3147
AlmuHS <almuhs@github.com>
2019-10-31 23:27:20 +0100

    experiment: stop cpu 1 in infinite loop

--- kern/sched_prim.c
+++ kern/sched_prim.c
@@ -1665,7 +1665,8 @@ void __attribute__((noreturn)) idle_thread_continue(void)
 #if	POWER_SAVE
 			if (mycpu != master_cpu){
                 printf("cpu %d entering in relax\n", mycpu);
-                for(i = 0; i < 10000000; i++);
+                //for(i = 0; i < 10000000; i++);
+                while(1);
                 printf("cpu %d out of relax", mycpu);
 			}
 			else{
