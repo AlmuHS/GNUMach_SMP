9a0bfb25b22a2ec9c2c170daf4416aecda4c4b59
AlmuHS <almuhs@github.com>
2019-10-31 23:50:32 +0100

    fix: set cpu 0 as bound processor

--- kern/thread.c
+++ kern/thread.c
@@ -1,25 +1,25 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1994-1987 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
@@ -361,7 +361,8 @@ void thread_init(void)
 	thread_template.ast = AST_ZILCH;
 
 	/* thread_template.processor_set (later) */
-	thread_template.bound_processor = PROCESSOR_NULL;
+	//thread_template.bound_processor = PROCESSOR_NULL;
+	thread_template.bound_processor = cpu_to_processor(0);
 #if	MACH_HOST
 	thread_template.may_assign = TRUE;
 	thread_template.assign_active = FALSE;
@@ -524,7 +525,7 @@ kern_return_t thread_create(
 	 *	processors queue to avoid master.
 	 */
 	if (!pset->empty) {
-		new_thread->last_processor = 
+		new_thread->last_processor =
 			(processor_t)queue_first(&pset->processors);
 	}
 	else {
@@ -812,7 +813,7 @@ kern_return_t thread_terminate(
 		thread_terminate(cur_thread);
 		return KERN_FAILURE;
 	}
-    
+
 	thread_unlock(cur_thread);
 	task_unlock(cur_task);
 
@@ -989,7 +990,7 @@ kern_return_t thread_halt(
 		}
 
 		thread_unlock(cur_thread);
-	
+
 	}
 	else {
 		/*
@@ -1037,7 +1038,7 @@ kern_return_t thread_halt(
 	/*
 	 *	Otherwise, have to do it ourselves.
 	 */
-		
+
 	thread_ast_set(thread, AST_HALT);
 
 	while (TRUE) {
@@ -1608,7 +1609,7 @@ kern_return_t thread_info(
 	    sched_info->base_priority = thread->priority;
 	    sched_info->max_priority = thread->max_priority;
 	    sched_info->cur_priority = thread->sched_pri;
-	    
+
 	    sched_info->depressed = (thread->depress_priority >= 0);
 	    sched_info->depress_priority = thread->depress_priority;
 
@@ -1637,7 +1638,7 @@ kern_return_t	thread_abort(
 
 	/*
 	 *
-         *	clear it of an event wait 
+         *	clear it of an event wait
          */
 	evc_notify_abort(thread);
 
@@ -1788,7 +1789,7 @@ thread_assign(
  *	thread_freeze:
  *
  *	Freeze thread's assignment.  Prelude to assigning thread.
- *	Only one freeze may be held per thread.  
+ *	Only one freeze may be held per thread.
  */
 void
 thread_freeze(
@@ -1850,7 +1851,7 @@ thread_doassign(
 	boolean_t			old_empty, new_empty;
 	boolean_t			recompute_pri = FALSE;
 	spl_t				s;
-	
+
 	/*
 	 *	Check for silly no-op.
 	 */
@@ -2000,7 +2001,7 @@ thread_assign_default(
  *	thread_get_assignment
  *
  *	Return current assignment for this thread.
- */	    
+ */
 kern_return_t thread_get_assignment(
 	thread_t	thread,
 	processor_set_t	*pset)
