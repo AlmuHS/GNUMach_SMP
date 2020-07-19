22015f544013728c3837d1e0c4cbece355b97e8a
Almudena García <almuhs@gitlab.com>
2020-07-19 18:25:19 +0200

    docs: added GPLv2 license and documented some functions

--- kern/smp.h
+++ kern/smp.h
@@ -1,8 +1,28 @@
+/* smp.h - Template for generic SMP controller for Mach. Header file
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   Written by Almudena Garcia Jurado-Centurion
+
+   This file is part of GNU Mach.
+
+   GNU Mach is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU Mach is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA. */
+
 struct smp_data{
     int num_cpus;
 };
 
 void smp_data_init(void);
-int get_numcpus(void);
-int get_current_cpu(void);
+int smp_get_numcpus(void);
+int smp_get_current_cpu(void);
 int smp_init(void);
