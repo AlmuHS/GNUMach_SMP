2bfe9d0ebde4b8d90201d0d74795093af70286cd
Almudena Garcia <liberamenso10000@gmail.com>
2020-07-27 16:00:38 +0200

    style: fixed format using mach style

--- i386/i386at/acpi_parse_apic.h
+++ i386/i386at/acpi_parse_apic.h
@@ -1,15 +1,16 @@
-/*Copyright 2018 Juan Bosco Garcia
- *This file is part of Min_SMP.
- *Min_SMP is free software: you can redistribute it and/or modify
- *it under the terms of the GNU General Public License as published by
- *the Free Software Foundation, either version 2 of the License, or
- *(at your option) any later version.
- *Min_SMP is distributed in the hope that it will be useful,
- *but WITHOUT ANY WARRANTY; without even the implied warranty of
- *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *GNU General Public License for more details.
- *You should have received a copy of the GNU General Public License
- *along with Min_SMP.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright 2018 Juan Bosco Garcia
+ * This file is part of Min_SMP.
+ * Min_SMP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * Min_SMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Min_SMP.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __ACPI_H__
@@ -23,11 +24,11 @@
 
 
 struct acpi_rsdp {
-    uint8_t signature[8];
-    uint8_t checksum;
-    uint8_t oem_id[6];
-    uint8_t revision[1];
-    uint32_t rsdt_addr;
+    uint8_t  	signature[8];
+    uint8_t  	checksum;
+    uint8_t  	oem_id[6];
+    uint8_t  	revision[1];
+    uint32_t 	rsdt_addr;
 } __attribute__((__packed__));
 
 
@@ -39,23 +40,23 @@ struct acpi_rsdp {
  * In MADT, the signature is 'APIC'
  */
 struct acpi_dhdr {
-    uint8_t signature[4];
-    uint32_t length;
-    uint8_t revision;
-    uint8_t checksum;
-    uint8_t oem_id[6];
-    uint8_t oem_table_id[8];
-    uint32_t oem_revision;
-    uint8_t creator_id[4];
-    uint32_t creator_revision;
+    uint8_t		signature[4];
+    uint32_t	length;
+    uint8_t  	revision;
+    uint8_t  	checksum;
+    uint8_t  	oem_id[6];
+    uint8_t  	oem_table_id[8];
+    uint32_t 	oem_revision;
+    uint8_t  	creator_id[4];
+    uint32_t 	creator_revision;
 } __attribute__((__packed__));
 
 
 #define ACPI_RSDT_SIG "RSDT"
 
 struct acpi_rsdt {
-    struct acpi_dhdr header;
-    uint32_t entry[0];
+    struct acpi_dhdr 	header;
+    uint32_t 			entry[0];
 } __attribute__((__packed__));
 
 /* APIC table signature */
@@ -72,8 +73,8 @@ struct acpi_rsdt {
  * Type: Local APIC (0), I/O APIC (1)
  */
 struct acpi_apic_dhdr {
-    uint8_t type;
-    uint8_t length;
+    uint8_t 	type;
+    uint8_t 	length;
 } __attribute__((__packed__));
 
 
@@ -85,9 +86,9 @@ struct acpi_apic_dhdr {
  * Entry field stores the descriptors of APIC structures
  */
 struct acpi_apic {
-    struct acpi_dhdr header; //Header, which stores the descriptor for RDST's Entry field
-    uint32_t lapic_addr; //Local Interrupt Controller Address
-    uint32_t flags;
+    struct   	acpi_dhdr header; //Header, which stores the descriptor for RDST's Entry field
+    uint32_t 	lapic_addr; //Local Interrupt Controller Address
+    uint32_t 	flags;
     struct acpi_apic_dhdr entry[0]; //Interrupt Controller Structure
 } __attribute__((__packed__));
 
@@ -97,10 +98,10 @@ struct acpi_apic {
  */
 
 struct acpi_apic_lapic {
-    struct acpi_apic_dhdr header;
-    uint8_t processor_id; //ACPI Processor UID
-    uint8_t apic_id;
-    uint32_t flags;
+    struct      acpi_apic_dhdr header;
+    uint8_t     processor_id; //ACPI Processor UID
+    uint8_t     apic_id;
+    uint32_t    flags;
 } __attribute__((__packed__));
 
 
@@ -110,24 +111,24 @@ struct acpi_apic_lapic {
  */
 
 struct acpi_apic_ioapic {
-    struct acpi_apic_dhdr header;
-    uint8_t apic_id;
-    uint8_t reserved;
-    uint32_t addr;
-    uint32_t base;
+    struct      acpi_apic_dhdr header;
+    uint8_t     apic_id;
+    uint8_t     reserved;
+    uint32_t    addr;
+    uint32_t    base;
 } __attribute__((__packed__));
 
 
 struct acpi_apic_irq_override {
     struct acpi_apic_dhdr header;
-    uint8_t bus;
-    uint8_t irq;
-    uint32_t gsi;
-    uint16_t flags;
+    uint8_t     bus;
+    uint8_t     irq;
+    uint32_t    gsi;
+    uint16_t    flags;
 } __attribute__((__packed__));
 
-int acpi_apic_init(void);
-void acpi_print_info(struct acpi_rsdp *rsdp, struct acpi_rsdt *rsdt, int acpi_rsdt_n);
+int acpi_apic_init (void);
+void acpi_print_info (struct acpi_rsdp *rsdp, struct acpi_rsdt *rsdt, int acpi_rsdt_n);
 
 
 #endif /* __ACPI_H__ */
