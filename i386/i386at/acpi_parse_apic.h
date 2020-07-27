69c8217f463ea42bdc50a36e92ed92af456ed2b2
Almudena Garcia <liberamenso10000@gmail.com>
2020-07-27 16:25:43 +0200

    refixed style

--- i386/i386at/acpi_parse_apic.h
+++ i386/i386at/acpi_parse_apic.h
@@ -16,8 +16,7 @@
 #ifndef __ACPI_H__
 #define __ACPI_H__
 
-#include <include/stdint.h> //uint16_t, uint32_t_t...
-
+#include <include/stdint.h>
 
 #define ACPI_RSDP_ALIGN 16
 #define ACPI_RSDP_SIG "RSD PTR "
@@ -32,12 +31,13 @@ struct acpi_rsdp {
 } __attribute__((__packed__));
 
 
-/* RSDT Entry Header
+/*
+ * RSDT Entry Header
  *
  * Header which stores the descriptors of tables pointed from RDSP's Entry Field
- * Includes the signature of the table, to identify each table
+ * Includes the signature of the table, to identify each table.
  *
- * In MADT, the signature is 'APIC'
+ * In MADT, the signature is 'APIC'.
  */
 struct acpi_dhdr {
     uint8_t		signature[4];
@@ -59,18 +59,19 @@ struct acpi_rsdt {
     uint32_t 			entry[0];
 } __attribute__((__packed__));
 
-/* APIC table signature */
+/* APIC table signature. */
 #define ACPI_APIC_SIG "APIC"
 
-/* Types value for MADT entries: Local APIC, IOAPIC and IRQ Override */
+/* Types value for MADT entries: Local APIC, IOAPIC and IRQ Override. */
 #define ACPI_APIC_ENTRY_LAPIC  0
 #define ACPI_APIC_ENTRY_IOAPIC 1
 #define ACPI_APIC_IRQ_OVERRIDE 2
 #define ACPI_APIC_ENTRY_NONMASK_IRQ 4
 
-/* APIC descriptor header
- * Define the type of the structure (Local APIC, I/O APIC or others)
- * Type: Local APIC (0), I/O APIC (1)
+/*
+ * APIC descriptor header
+ * Define the type of the structure (Local APIC, I/O APIC or others).
+ * Type: Local APIC (0), I/O APIC (1).
  */
 struct acpi_apic_dhdr {
     uint8_t 	type;
@@ -78,34 +79,37 @@ struct acpi_apic_dhdr {
 } __attribute__((__packed__));
 
 
-/* Multiple APIC Description Table (MADT)
+/*
+ * Multiple APIC Description Table (MADT)
  *
- * Describes the APIC structures which exist in the machine
- * Includes the common address where Local APIC is mapped in main memory
+ * Describes the APIC structures which exist in the machine.
+ * Includes the common address where Local APIC is mapped in main memory.
  *
- * Entry field stores the descriptors of APIC structures
+ * Entry field stores the descriptors of APIC structures.
  */
 struct acpi_apic {
-    struct   	acpi_dhdr header; //Header, which stores the descriptor for RDST's Entry field
-    uint32_t 	lapic_addr; //Local Interrupt Controller Address
+    struct   	acpi_dhdr header;       /* Header, which stores the descriptor for RDST's Entry field. */
+    uint32_t 	lapic_addr;             /* Local Interrupt Controller Address. */
     uint32_t 	flags;
-    struct acpi_apic_dhdr entry[0]; //Interrupt Controller Structure
+    struct acpi_apic_dhdr entry[0];     /* Interrupt Controller Structure */
 } __attribute__((__packed__));
 
-/* Processor Local APIC Structure
+/*
+ * Processor Local APIC Structure
  *
  * Stores information about APIC ID, flags and ACPI Processor UID
  */
 
 struct acpi_apic_lapic {
     struct      acpi_apic_dhdr header;
-    uint8_t     processor_id; //ACPI Processor UID
+    uint8_t     processor_id;           /* ACPI Processor UID */
     uint8_t     apic_id;
     uint32_t    flags;
 } __attribute__((__packed__));
 
 
-/* I/O APIC Structure
+/*
+ * I/O APIC Structure
  *
  * Stores information about APIC ID, and I/O APIC tables
  */
@@ -118,6 +122,11 @@ struct acpi_apic_ioapic {
     uint32_t    base;
 } __attribute__((__packed__));
 
+/*
+ * IRQ Override structure
+ *
+ * Stores information about IRQ override, with busses and IRQ.
+ */
 
 struct acpi_apic_irq_override {
     struct acpi_apic_dhdr header;
