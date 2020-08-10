a9edb4023087bfb14f1a915976c189a012893fb4
Almudena Garcia <liberamenso10000@gmail.com>
2020-08-11 01:43:57 +0200

    format: fix format in ApicLocalUnit address comments

--- i386/i386/apic.h
+++ i386/i386/apic.h
@@ -39,82 +39,44 @@ typedef struct ApicIoUnit {
 
 
 typedef struct ApicLocalUnit {
-        /* 0x000 */
-        ApicReg reserved0;
-        /* 0x010 */
-        ApicReg reserved1;
-        /* 0x020 */
-        ApicReg apic_id;
-        /* 0x030 */
-        ApicReg version;
-        /* 0x040 */
-        ApicReg reserved4;
-        /* 0x050 */
-        ApicReg reserved5;
-        /* 0x060 */
-        ApicReg reserved6;
-        /* 0x070 */
-        ApicReg reserved7;
-        /* 0x080 */
-        ApicReg task_pri;
-        /* 0x090 */
-        ApicReg arbitration_pri;
-        /* 0x0a0 */
-        ApicReg processor_pri;
-        /* 0x0b0 */
-        ApicReg eoi;
-        /* 0x0c0 */
-        ApicReg remote;
-        /* 0x0d0 */
-        ApicReg logical_dest;
-        /* 0x0e0 */
-        ApicReg dest_format;
-        /* 0x0f0 */
-        ApicReg spurious_vector;
-        /* 0x100 */
-        ApicReg isr[8];
-        /* 0x180 */
-        ApicReg tmr[8];
-        /* 0x200 */
-        ApicReg irr[8];
-        /* 0x280 */
-        ApicReg error_status;
-        /* 0x290 */
-        ApicReg reserved28[6];
-        /* 0x2f0 */
-        ApicReg lvt_cmci;
-        /* 0x300 */
-        ApicReg icr_low;
-        /* 0x310 */
-        ApicReg icr_high;
-        /* 0x320 */
-        ApicReg lvt_timer;
-        /* 0x330 */
-        ApicReg lvt_thermal;
-        /* 0x340 */
-        ApicReg lvt_performance_monitor;
-        /* 0x350 */
-        ApicReg lvt_lint0;
-        /* 0x360 */
-        ApicReg lvt_lint1;
-        /* 0x370 */
-        ApicReg lvt_error;
-        /* 0x380 */
-        ApicReg init_count;
-        /* 0x390 */
-        ApicReg cur_count;
-        /* 0x3a0 */
-        ApicReg reserved3a;
-        /* 0x3b0 */
-        ApicReg reserved3b;
-        /* 0x3c0 */
-        ApicReg reserved3c;
-        /* 0x3d0 */
-        ApicReg reserved3d;
-        /* 0x3e0 */
-        ApicReg divider_config;
-        /* 0x3f0 */
-        ApicReg reserved3f;
+        ApicReg reserved0;               /* 0x000 */
+        ApicReg reserved1;               /* 0x010 */
+        ApicReg apic_id;                 /* 0x020 */
+        ApicReg version;                 /* 0x030 */
+        ApicReg reserved4;               /* 0x040 */
+        ApicReg reserved5;               /* 0x050 */
+        ApicReg reserved6;               /* 0x060 */
+        ApicReg reserved7;               /* 0x070 */
+        ApicReg task_pri;                /* 0x080 */
+        ApicReg arbitration_pri;         /* 0x090 */
+        ApicReg processor_pri;           /* 0x0a0 */
+        ApicReg eoi;                     /* 0x0b0 */
+        ApicReg remote;                  /* 0x0c0 */
+        ApicReg logical_dest;            /* 0x0d0 */
+        ApicReg dest_format;             /* 0x0e0 */
+        ApicReg spurious_vector;         /* 0x0f0 */
+        ApicReg isr[8];                  /* 0x100 */
+        ApicReg tmr[8];                  /* 0x180 */
+        ApicReg irr[8];                  /* 0x200 */
+        ApicReg error_status;            /* 0x280 */
+        ApicReg reserved28[6];           /* 0x290 */
+        ApicReg lvt_cmci;                /* 0x2f0 */
+        ApicReg icr_low;                 /* 0x300 */
+        ApicReg icr_high;                /* 0x310 */
+        ApicReg lvt_timer;               /* 0x320 */
+        ApicReg lvt_thermal;             /* 0x330 */
+        ApicReg lvt_performance_monitor; /* 0x340 */
+        ApicReg lvt_lint0;               /* 0x350 */
+        ApicReg lvt_lint1;               /* 0x360 */
+        ApicReg lvt_error;               /* 0x370 */
+        ApicReg init_count;              /* 0x380 */
+        ApicReg cur_count;               /* 0x390 */
+        ApicReg reserved3a;              /* 0x3a0 */
+        ApicReg reserved3b;              /* 0x3b0 */
+        ApicReg reserved3c;              /* 0x3c0 */
+        ApicReg reserved3d;              /* 0x3d0 */
+        ApicReg divider_config;          /* 0x3e0 */
+        ApicReg reserved3f;              /* 0x3f0 */
 } ApicLocalUnit;
 
 typedef struct IoApicData {
@@ -148,7 +110,7 @@ void apic_lapic_init(ApicLocalUnit* lapic_ptr);
 void apic_add_ioapic(struct IoApicData);
 void apic_add_irq_override(struct IrqOverrideData irq_over);
 uint16_t apic_get_cpu_apic_id(int kernel_id);
-ApicLocalUnit* apic_get_lapic(void);
+volatile ApicLocalUnit* apic_get_lapic(void);
 struct IoApicData apic_get_ioapic(int kernel_id);
 uint8_t apic_get_numcpus(void);
 uint8_t apic_get_num_ioapics(void);
