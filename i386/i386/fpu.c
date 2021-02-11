307130477110d33ab193f4f5376847d2234c1aca
Andrea G. Monaco <andrea.monaco@autistici.org>
2021-02-11 13:31:31 +0100

    i386 fpu: fix bogus check
    Avoid panicing on all CPUs that do not support xsaves.
    
    * i386/i386/fpu.c (init_fpu): Panic on CPUs that do not support xsaves
    only if the cpu-reported xsave size is smaller than expected.

--- i386/i386/fpu.c
+++ i386/i386/fpu.c
@@ -191,7 +191,7 @@ init_fpu(void)
 			ecx = 0x0;
 			cpuid(eax, ebx, ecx, edx);
 			fp_xsave_size = offsetof(struct i386_fpsave_state, xfp_save_state) + ebx;
-			if(fp_xsave_size < sizeof(struct i386_fpsave_state));
+			if(fp_xsave_size < sizeof(struct i386_fpsave_state))
 				panic("CPU-provided xstate size %d "
 				      "is smaller than our minimum %d!\n",
 				      fp_xsave_size,
