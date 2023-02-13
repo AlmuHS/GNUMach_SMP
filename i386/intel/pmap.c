f9ebee5589f8056ef52c6ecfdf7d6e10f4d34d1c
Damien Zammit <damien@zamaudio.com>
2023-02-13 08:49:45 +0000

    pmap: Signal cpu for TLB update if kernel_pmap
    Message-Id: <20230213084919.1157074-3-damien@zamaudio.com>

--- i386/intel/pmap.c
+++ i386/intel/pmap.c
@@ -3013,7 +3013,7 @@ void    signal_cpus(
 	    cpu_update_needed[which_cpu] = TRUE;
 	    simple_unlock(&update_list_p->lock);
 
-	    if ((cpus_idle & (1 << which_cpu)) == 0)
+	    if (((cpus_idle & (1 << which_cpu)) == 0) || (pmap == kernel_pmap))
 		interrupt_processor(which_cpu);
 	    use_list &= ~(1 << which_cpu);
 	}
