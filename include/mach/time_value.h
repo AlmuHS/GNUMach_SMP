984576eb09fdc554a934fb62c404c1b69b3ee68b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2023-03-13 20:33:13 +0100

    Drop spurious tabs

--- include/mach/time_value.h
+++ include/mach/time_value.h
@@ -128,30 +128,30 @@ static __inline__ time_value_t convert_time_value_from_user(rpc_time_value_t tv)
     time_value_add_usec(result, (addend)->microseconds);	\
   }
 
-#define	time_value64_add(result, addend) {									\
-    time_value64_assert(addend);														\
-    (result)->seconds += (addend)->seconds;									\
+#define	time_value64_add(result, addend) {			\
+    time_value64_assert(addend);				\
+    (result)->seconds += (addend)->seconds;			\
     time_value64_add_nanos(result, (addend)->nanoseconds);	\
   }
 
-#define	time_value64_sub(result, subtrahend) {									\
-    time_value64_assert(subtrahend);														\
-    (result)->seconds -= (subtrahend)->seconds;									\
+#define	time_value64_sub(result, subtrahend) {			\
+    time_value64_assert(subtrahend);				\
+    (result)->seconds -= (subtrahend)->seconds;			\
     time_value64_sub_nanos(result, (subtrahend)->nanoseconds);	\
   }
 
-#define time_value64_init(tv)	{												\
-		(tv)->seconds = 0;																\
-		(tv)->nanoseconds = 0;														\
+#define time_value64_init(tv)	{				\
+		(tv)->seconds = 0;				\
+		(tv)->nanoseconds = 0;				\
 	}
 
 #define TIME_VALUE64_TO_TIME_VALUE(tv64, tv) do {			\
-		(tv)->seconds = (tv64)->seconds;									\
+		(tv)->seconds = (tv64)->seconds;			\
 		(tv)->microseconds = (tv64)->nanoseconds / 1000;	\
 } while(0)
 
 #define TIME_VALUE_TO_TIME_VALUE64(tv, tv64) do {			\
-		(tv64)->seconds = (tv)->seconds;									\
+		(tv64)->seconds = (tv)->seconds;			\
 		(tv64)->nanoseconds = (tv)->microseconds * 1000;	\
 } while(0)
 
